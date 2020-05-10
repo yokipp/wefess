6666771bc2a0f1fc2ab08158620dba1df86ad408
WeFess <yokie.gams@gmail.com>
2020-05-10 22:02:07 +0700

    update

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ def start():
                 id = dms[i]['id']
 
                 if len(message) is not 0 and len(message) < 280:
-                    if "wf" in message:
+                    if "wf" in message.lower():
                         message = message.replace("wf", "")
                         if len(message) is not 0:
                             if dms[i]['media'] is None:
