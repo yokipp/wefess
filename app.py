fccddea03f5539226aee7e49e793d18bf44df524
WeFess <yokie.gams@gmail.com>
2020-05-10 22:14:03 +0700

    update

--- app.py
+++ app.py
@@ -17,8 +17,8 @@ def start():
                 id = dms[i]['id']
 
                 if len(message) is not 0 and len(message) < 280:
-                    if "wf" in message.lower():
-                        message = message.replace("wf", "")
+                    if "wf!" in message.lower():
+                        message = message.replace("wf!", "Wf!")
                         if len(message) is not 0:
                             if dms[i]['media'] is None:
                                 print("DM will be posted")
